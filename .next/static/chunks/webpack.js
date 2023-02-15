7db3e6c01a7f2cd984c4faddf34baefa884e5bf2
salscodes <salissadiq@gmail.com>
2023-02-15 16:04:55 +0100

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2cc01c112a78080e"; }
+/******/ 		__webpack_require__.h = function() { return "08990ec803db83ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
